fdfa21beaba9b5737f45b5d00da75e756f1387ec
danielgrippi <danielgrippi@gmail.com>
2011-07-13 13:37:36 -0700

    user can retract a reshared post

--- app/models/user.rb
+++ app/models/user.rb
@@ -223,7 +223,12 @@ class User < ActiveRecord::Base
       retraction = Retraction.for(target)
     end
 
-    mailman = Postzord::Dispatch.new(self, retraction)
+    opts = {}
+    if target.is_a?(Post)
+      opts[:additional_subscribers] = target.resharers
+    end
+
+    mailman = Postzord::Dispatch.new(self, retraction, opts)
     mailman.post
 
     retraction.perform(self)
